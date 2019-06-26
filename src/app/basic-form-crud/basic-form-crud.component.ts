dc8f547aa8ceaa3310b9fdd53f91abb6a2dacd8f
arnabbhat82 <arnabbhat@gmail.com>
2019-06-26 12:40:08 +0530

    hasError validation

--- src/app/basic-form-crud/basic-form-crud.component.ts
+++ src/app/basic-form-crud/basic-form-crud.component.ts
@@ -10,10 +10,10 @@ export class BasicFormCrudComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   productForm = this.fb.group({
-    name: ['', Validators.required, Validators.minLength(3)],
-    price: ['', Validators.required, Validators.min(0)],
-    details: ['', Validators.required, Validators.minLength(10)],
-    quantityAvailable: ['', Validators.required, Validators.min(0)]
+    name: [null, Validators.required, Validators.minLength(3)],
+    price: [null, Validators.required, Validators.min(0)],
+    details: [null, Validators.required, Validators.minLength(10)],
+    quantityAvailable: [null, Validators.required, Validators.min(0)]
   });
 
   get name() {
